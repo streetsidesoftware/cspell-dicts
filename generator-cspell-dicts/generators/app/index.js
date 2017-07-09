9176c381c6906d29ef5ad2258b7f3e3d3757aaeb
Jason Dent <jason@streetsidesoftware.nl>
2017-07-09 16:16:42 +0200

    Update generator to copy files and take a dictionary name.

--- generator-cspell-dicts/generators/app/index.js
+++ generator-cspell-dicts/generators/app/index.js
@@ -3,49 +3,86 @@ const Generator = require('yeoman-generator');
 const chalk = require('chalk');
 const yosay = require('yosay');
 const path = require('path');
+const mkdirp = require('mkdirp');
 
 module.exports = class extends Generator {
+  constructor(args, opts) {
+    super(args, opts);
+
+    this.option('source', {
+      desc: 'Source file for the dictionary. It will be copied to into the local dictionary folder.',
+      type: String,
+      required: false,
+      alias: 's'
+    });
+
+    this.argument('name', {
+      desc: 'Name of Dictionary',
+      type: String,
+      required: true
+    });
+  }
+
   prompting() {
     // Have Yeoman greet the user.
     this.log(yosay(
-      'Welcome to the ' + chalk.red('generator-cspell-dicts') + ' generator!'
+      'Welcome to the ' + chalk.red('cspell-dicts') + ' generator!'
     ));
 
-    const prompts = [
+    const namePrompts = [
       {
         type: 'input',
         name: 'name',
         message: 'Your project name',
-        default: this.appname // Default to current folder name
-      },
-      {
-        type: 'input',
-        name: 'description',
-        message: 'Description',
-        default: this.appname[0].toUpperCase() + this.appname.slice(1) + ' dictionary for cspell.'
-      },
-      {
-        type: 'input',
-        name: 'srcFile',
-        message: 'Source File Name',
-        default: this.appname + '.txt'
-      },
-      {
-        type: 'confirm',
-        name: 'useTrie',
-        message: 'Store as Trie',
-        default: true
+        default: this.options.name || this.appname // Default to current folder name
       }
     ];
 
-    return this.prompt(prompts).then(props => {
-      // To access props later use this.props.someAnswer;
+    return this.prompt(namePrompts).then(props => {
+      return this.prompt([
+        {
+          type: 'input',
+          name: 'description',
+          message: 'Description',
+          default: title(props.name) + ' dictionary for cspell.'
+        },
+        {
+          type: 'input',
+          name: 'srcFile',
+          message: 'Source File Name',
+          default: this.options.source || props.name + '.txt'
+        },
+        {
+          type: 'confirm',
+          name: 'useTrie',
+          message: 'Store as Trie',
+          default: true
+        }
+      ]).then(depProps => {
+        depProps.fileExt = depProps.useTrie ? 'trie.gz' : 'txt.gz';
+        depProps.command = depProps.useTrie ? 'compile-trie' : 'compile';
 
-      props.fileExt = props.useTrie ? 'trie.gz' : 'txt.gz';
-      props.command = props.useTrie ? 'compile-trie' : 'compile';
-      props.dstFileName = path.basename(props.srcFile, props.srcFile.replace(/^.*(\.\w+)$/, '$1')) +
-        '.' + props.fileExt;
-      this.props = props;
+        depProps.dstFileName = path.basename(depProps.srcFile, path.extname(depProps.srcFile)) +
+          '.' + depProps.fileExt;
+
+        props.filesToCopy = [];
+        const srcFile = path.resolve(depProps.srcFile);
+        this.log(srcFile);
+
+        if (this.fs.exists(srcFile)) {
+          props.filesToCopy.push(srcFile);
+          if (path.extname(srcFile) === '.dic') {
+            props.filesToCopy.push(path.join(
+              path.dirname(srcFile),
+              path.basename(srcFile, '.dic') + '.aff'
+            ));
+          }
+        }
+
+        depProps.srcFile = path.basename(srcFile);
+
+        this.props = Object.assign({}, props, depProps);
+      });
     });
   }
 
@@ -60,6 +97,22 @@ module.exports = class extends Generator {
       this.destinationPath('README.md'),
       this.props
     );
+    this.props.filesToCopy.forEach(name => {
+      this.fs.copy(
+        name,
+        this.destinationPath(path.basename(name))
+      );
+    });
+  }
+
+  default() {
+    if (path.basename(this.destinationPath()) !== this.props.name) {
+      this.log(
+        'Creating Folder: ' + this.props.name
+      );
+      mkdirp(this.props.name);
+      this.destinationRoot(this.destinationPath(this.props.name));
+    }
   }
 
   install() {
@@ -70,3 +123,8 @@ module.exports = class extends Generator {
     });
   }
 };
+
+function title(s) {
+  return s[0].toUpperCase() + s.slice(1);
+}
+
--- generator-cspell-dicts/generators/app/templates/README.md
+++ generator-cspell-dicts/generators/app/templates/README.md
@@ -1,5 +1,7 @@
 # Cspell <%= name %> Dictionary
 
+This is a pre-built dictionary for use with cspell.
+
 ## Building
 
 Building is only necessary if you want to modify the contents of the dictionary.  Note: Building will take a few minutes for large files.
