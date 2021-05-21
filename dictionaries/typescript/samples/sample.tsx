ba4918df1366d552424a14670fd6ea43ceb01bc4
Jason Dent <jason@streetsidesoftware.nl>
2021-05-21 15:46:42 +0200

    fix: #421

---
+++ dictionaries/typescript/samples/sample.tsx
@@ -0,0 +1,11 @@
+// Sample Typescript React file.
+
+// hex escape example
+const root = '\x3croot\x3e';
+
+// unicode escape example
+const other = '\u002aword'
+
+function main() {
+    console.log(root);
+}
