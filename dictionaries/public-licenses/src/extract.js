99289df0ca5f989811952e95692e30cfb2e21bd5
Jason Dent <Jason3S@users.noreply.github.com>
2021-08-14 22:52:22 +0200

    dev: Add new public-licenses dictionary (#548)
    * dev: Add new public-licenses dictionary
    * dev: Update dictionary templates
    * dev: add some terms
    * Update test.yml to use Node 16

---
+++ dictionaries/public-licenses/src/extract.js
@@ -0,0 +1,39 @@
+/**
+ * Generate the list of public licenses based upon spdx-license-ids.
+ */
+const fs = require('fs');
+const path = require('path');
+const ids = require('spdx-license-ids');
+
+const filename = path.join(__dirname, './generated/public-licenses.txt');
+
+function processIds() {
+    const compare = new Intl.Collator().compare;
+
+    const collection = new Set(ids);
+
+    for (const line of ids) {
+        const a = line
+            .replace(/[.0-9]/g, ' ')
+            .split(' ')
+            .map((a) => a.replace(/^-+/, ''))
+            .map((a) => a.replace(/-+$/, ''))
+            .map((a) => a.replace(/^.{1,3}$/, ''))
+            .filter((a) => !!a);
+        a.forEach((w) => collection.add(w));
+    }
+
+    const words = [...collection].sort(compare);
+    const text = `# Generated from spdx-license-ids
+${words.join('\n')}
+
+`;
+
+    return fs.writeFile(filename, text, 'utf-8', (err) => {
+        if (err) {
+            throw err;
+        }
+    });
+}
+
+processIds();
