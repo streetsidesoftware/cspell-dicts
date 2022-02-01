f13a13f7715fb5a04930581bd1a1ace5b952f3f7
Matthew Toohey <contact@mtoohey.com>
2022-02-01 12:07:27 -0500

    fix(r): capture names from all default packages

--- dictionaries/r/scripts/generate.r
+++ dictionaries/r/scripts/generate.r
@@ -1,2 +1,11 @@
-# cspell:ignore globalenv
-writeLines(ls(parent.env(globalenv())), file("../src/r.txt", "wt"))
+out_file <- file("../src/r.txt", "wt")
+defaultPackages <- getOption("defaultPackages")
+
+for (package in defaultPackages) {
+    writeLines(
+        ls(as.environment(paste("package", package, sep = ":"))),
+        out_file
+    )
+}
+writeLines(ls(baseenv()), out_file)
+close(out_file)
