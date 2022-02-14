0913e90b75fdd2b278f84978bb838c946054d0ae
Jason Dent <Jason3S@users.noreply.github.com>
2022-02-14 20:38:20 +0100

    fix: Prepare Dart for publishing (#924)

---
+++ dictionaries/dart/samples/sample.dart
@@ -0,0 +1,39 @@
+/**
+ * From [Language samples | Dart](https://dart.dev/samples)
+ */
+
+/** Variables */
+
+var name = 'Voyager I';
+var year = 1977;
+var antennaDiameter = 3.7;
+var flybyObjects = ['Jupiter', 'Saturn', 'Uranus', 'Neptune'];
+var image = {
+  'tags': ['saturn'],
+  'url': '//path/to/saturn.jpg'
+};
+
+/** Control flow statements */
+
+if (year >= 2001) {
+  print('21st century');
+} else if (year >= 1901) {
+  print('20th century');
+}
+
+for (final object in flybyObjects) {
+  print(object);
+}
+
+for (int month = 1; month <= 12; month++) {
+  print(month);
+}
+
+while (year < 2016) {
+  year += 1;
+}
+
+/** Hello World */
+void main() {
+  print('Hello, World!');
+}
