0cb26602788e42ef42d0eb639c38be2abb0450c3
Jason Dent <jason@streetsidesoftware.nl>
2022-05-02 09:50:01 +0200

    fix: Add common python compounds
    Source Django code base.

--- dictionaries/python/scripts/fetch-python.sh
+++ dictionaries/python/scripts/fetch-python.sh
@@ -3,4 +3,4 @@
 rm -rf temp
 mkdir -p temp/python/cpython
 git clone --single-branch --depth 1 https://github.com/python/cpython.git temp/python/cpython
-python scripts/extractLib.py > src/python-lib.txt
+python scripts/extractLib.py > src/python/python-lib.txt
