c5abbd3fa57bb392f074960c80ec1fe27989de4e
Jason Dent <jason@streetsidesoftware.nl>
2024-07-04 17:56:58 +0200

    chore: fix release-please

--- scripts/gen-release-please-config.sh
+++ scripts/gen-release-please-config.sh
@@ -5,7 +5,7 @@ JQ_FILTER_PACKAGES="$SCRIPT_DIR/gen-release-please-config-packages.jq"
 JQ_FILTER_CONFIG="$SCRIPT_DIR/gen-release-please-config.jq"
 
 
-echo "./package.json" $(ls -1 dictionaries/*/package.json) \
+echo "./package.json" $(ls -1 dictionaries/*/package.json) $(ls -1 packages/*/package.json)  \
     | xargs jq -f $JQ_FILTER_PACKAGES | jq -s add | jq -f $JQ_FILTER_CONFIG > release-please-config.json
 
 pnpm prettier -w r*.json
