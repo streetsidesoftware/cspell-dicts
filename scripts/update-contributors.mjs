0a7499be5aeed6a998c6713c3055c324d98ef3c0
Jason Dent <jason@streetsidesoftware.nl>
2024-09-22 14:15:20 +0200

    chore: improve contrib script

--- scripts/update-contributors.mjs
+++ scripts/update-contributors.mjs
@@ -1,12 +1,20 @@
 #!/usr/bin/env node
 
-import { execSync } from 'node:child_process';
-import { writeFile } from 'node:fs/promises';
+// @ts-check
 
-import { fetchContributors } from './fetch-contributors.mjs';
+import { execSync } from 'node:child_process';
+import { readFile, writeFile } from 'node:fs/promises';
+
+import { fetchContributors, normalizeContributors, sortContributorsByLogin } from './lib/fetch-contributors.mjs';
+
+/**
+ * @typedef {Awaited<ReturnType<typeof fetchContributors>>} Contributors
+ * @typedef {Contributors[0]} Contributor
+ */
 
 const token = process.argv[2] || (await getToken());
 
+const contributorsFileUrl = new URL('../static/contributors.json', import.meta.url);
 const outputFileUrl = new URL('../static/contributors.md', import.meta.url);
 
 async function getToken() {
@@ -36,14 +44,44 @@ function contributorsToMd(contributors) {
     );
 }
 
+/**
+ * Read the list previous of contributors.
+ * @returns {Promise<Contributor[]>}
+ */
+async function readContributors() {
+    try {
+        const content = await readFile(contributorsFileUrl, 'utf-8');
+        return JSON.parse(content);
+    } catch {
+        return [];
+    }
+}
+
+/**
+ * Write the contributors to the output files.
+ * @param {Contributor[]} contributors
+ */
+async function writeContributors(contributors) {
+    await writeFile(outputFileUrl, contributorsToMd(contributors));
+    await writeFile(contributorsFileUrl, JSON.stringify(sortContributorsByLogin([...contributors]), null, 2));
+}
+
 async function run() {
     if (!token) {
         throw new Error('GitHub token is required');
     }
 
-    const contributors = await fetchContributors(token);
+    const previousContributors = await readContributors();
+    const currentContributors = normalizeContributors(await fetchContributors(token)).filter((c) => c.type !== 'Bot');
 
-    await writeFile(outputFileUrl, contributorsToMd(contributors));
+    const byLogin = new Map([...previousContributors, ...currentContributors].map((c) => [c.login, c]));
+    const logins = [...new Set([...currentContributors, ...previousContributors].map((c) => c.login))];
+    const ordered = new Map(logins.map((login, i) => [login, i]));
+    const contributors = [...byLogin.values()].sort(
+        (a, b) => (ordered.get(a.login) || 0) - (ordered.get(b.login) || 0),
+    );
+
+    await writeContributors(contributors);
 }
 
 run();
