9acc823eb836c106a93843b93982c13043d22a2d
Jason Dent <Jason3S@users.noreply.github.com>
2025-06-11 17:35:53 +0200

    feat: Add typos from crate-ci (#4538)

---
+++ dictionaries/en-common-misspellings/scripts/transform-crate-ci-typos.mjs
@@ -0,0 +1,111 @@
+#!/usr/bin/env node
+
+// ts-check
+import fs from 'node:fs/promises';
+
+import { readFileText } from 'cspell-io';
+import { decodeTrie, parseDictionary } from 'cspell-trie-lib';
+
+const root = new URL('../', import.meta.url);
+
+const typesDirURL = new URL('src/crate-ci/typos/', root);
+const outdirURL = new URL('src/fromCrateTypos/', root);
+
+const dictionaryUrls = {
+    'en-US': new URL('../en_US/en_US.trie', root),
+    'en-GB': new URL('../en_GB/en_GB.trie', root),
+    softwareTerms: new URL('../software-terms/dict/softwareTerms.txt', root),
+    exclude: new URL('src/excludeWords.txt', root),
+};
+
+function getHeader(locale) {
+    return `\
+# This file is generated from the words.csv
+# Add additional suggestions here.
+# the form is:
+# misspelling->suggestion
+#
+# cspell\x3Alocale ${locale}
+#
+# Tell cspell to not spell check the misspelled words:
+# cspell\x3AignoreRegExp /\\b[\\p{L}'-]+->/gmu
+#
+`;
+}
+
+async function loadDictionaryFromFile(fileName) {
+    const file = await readFileText(fileName);
+    const trie = /\.trie\b/.test(fileName) ? decodeTrie(file) : parseDictionary(file);
+    return trie;
+}
+
+async function loadDictionaries() {
+    return {
+        'en-US': await loadDictionaryFromFile(dictionaryUrls['en-US']),
+        'en-GB': await loadDictionaryFromFile(dictionaryUrls['en-GB']),
+        softwareTerms: await loadDictionaryFromFile(dictionaryUrls.softwareTerms),
+        exclude: await loadDictionaryFromFile(dictionaryUrls.exclude),
+    };
+}
+
+async function readTyposEntries() {
+    const file = await readFileText(new URL('words.csv', typesDirURL));
+    return file
+        .split(/\r?\n/g)
+        .map((line) => line.trim())
+        .map((line) => line.split(',').map((word) => word.trim()))
+        .map((words) => ({ word: words[0], suggestions: words.slice(1) }));
+}
+
+function processEntry(entry, dict, excludeDict) {
+    // if (dict.has(entry.word)) {
+    //     return undefined; // Skip if the word is already in the dictionary
+    // }
+    const suggestions = entry.suggestions.filter((sug) => !excludeDict.has(sug) && dict.has(sug));
+    if (!suggestions.length) {
+        return undefined; // Skip if no valid suggestions
+    }
+    return `${entry.word}->${suggestions.join(',')}`;
+}
+
+async function writeEntriesToFile(url, entries, locale) {
+    const content = getHeader(locale) + [...entries].sort().join('\n') + '\n';
+    await fs.writeFile(url, content, 'utf8');
+}
+
+async function main() {
+    const dictionaries = await loadDictionaries();
+    const typosEntries = await readTyposEntries();
+    const entriesEnAll = new Set();
+    const entriesEnUS = new Set();
+    const entriesEnGB = new Set();
+    const entriesSoftwareTerms = new Set();
+
+    for (const entry of typosEntries) {
+        const enUS = processEntry(entry, dictionaries['en-US'], dictionaries.exclude);
+        const enGB = processEntry(entry, dictionaries['en-GB'], dictionaries.exclude);
+        if (enUS === enGB) {
+            if (enUS) {
+                entriesEnAll.add(enUS);
+            }
+        } else {
+            if (enUS) {
+                entriesEnUS.add(enUS);
+            }
+            if (enGB) {
+                entriesEnGB.add(enGB);
+            }
+        }
+        const softwareTerms = processEntry(entry, dictionaries.softwareTerms, dictionaries.exclude);
+        if (softwareTerms && softwareTerms !== enUS) {
+            entriesSoftwareTerms.add(softwareTerms);
+        }
+    }
+
+    await writeEntriesToFile(new URL('dict-en-us.txt', outdirURL), entriesEnUS, 'en-US');
+    await writeEntriesToFile(new URL('dict-en-gb.txt', outdirURL), entriesEnGB, 'en-GB');
+    await writeEntriesToFile(new URL('dict-en.txt', outdirURL), entriesEnAll, 'en');
+    await writeEntriesToFile(new URL('softwareTerms.txt', outdirURL), entriesSoftwareTerms, 'en');
+}
+
+main();
---
+++ dictionaries/en-common-misspellings/src/crate-ci/typos/README.md
@@ -0,0 +1,3 @@
+# Words from [crate-ci/typos: Source code spell checker](https://github.com/crate-ci/typos)
+
+This file contains suggestions from the Typos repo.
