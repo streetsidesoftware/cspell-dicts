fd0ef0a3035fcb2d29966806ad0b2afbc342da9e
Jason Dent <jason@streetsidesoftware.nl>
2026-01-24 09:45:51 +0100

    feat!: Use yaml file for Latex Patterns

--- cspell-dict-file-checker/check.mjs
+++ cspell-dict-file-checker/check.mjs
@@ -1,7 +1,12 @@
 // @ts-check
 import test from 'ava';
 import { checkText } from 'cspell';
+import { spellCheckFile } from 'cspell-lib';
 import { glob } from 'glob';
+
+/**
+ * @typedef { import("cspell-lib").ValidationIssue } ValidationIssue
+ */
 
 /**
  *
@@ -14,7 +19,7 @@ function rightJustify(val, n = 5) {
 }
 
 /**
- * @param {import("cspell").CheckTextResult } result
+ * @param { import("cspell").CheckTextResult } result
  */
 function formatResult(result) {
     return result.items.map(
@@ -37,6 +42,58 @@ export function checkSnapshots(fileGlobsToCheck) {
             test(`verify ${filename} against snapshot`, async (t) => {
                 const result = await checkText(filename, {});
                 t.snapshot(formatResult(result));
+            });
+        });
+    }
+}
+
+/**
+ * @typedef { { text: string, line: number, col: number } } Issue
+ */
+
+/**
+ *
+ * @param {string} text
+ * @param {ValidationIssue[]} issues
+ * @return { Issue[] }
+ */
+function formatValidationIssues(text, issues) {
+    issues.sort((a, b) => a.offset - b.offset);
+
+    /** @type { Issue[] } */
+    const results = [];
+
+    let line = 1;
+    let lineStart = 0;
+    let j = 0;
+
+    for (let i = 0; i < text.length && j < issues.length; i++) {
+        if (text[i] === '\n') {
+            line++;
+            lineStart = i + 1;
+        }
+        while (j < issues.length && issues[j].offset === i) {
+            results.push({ text: issues[j].text, line, col: i - lineStart + 1 });
+            j++;
+        }
+    }
+
+    return results;
+}
+
+/**
+ * @param {string[]} fileGlobsToCheck
+ */
+export function spellcheckFilesAgainstSnapshot(fileGlobsToCheck) {
+    for (const fileGlob of fileGlobsToCheck) {
+        const globs = glob.sync(fileGlob);
+
+        globs.forEach((filename) => {
+            filename = filename.replace(/\\/g, '/');
+            test(`Spell check ${filename} issues against snapshot`, async (t) => {
+                const result = await spellCheckFile(filename, {}, {});
+                const text = result.document.text || '';
+                t.snapshot(formatValidationIssues(text, result.issues));
             });
         });
     }
